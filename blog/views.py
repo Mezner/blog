fe7b9f7247b29f8697e94d78ba1303da64df9c07
Russell Myers <mezner@russellmyers.com>
2012-02-27 23:05:08 -0500

    Implementing index and view_post view definitions.

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from blog.models import Post
+
+def index(request):
+    return render_to_response('blog/index.html', {
+        'posts': Post.objects.all()
+    })
+
+def view_post(request, slug):
+    return render_to_response('blog/view_post.html', {
+        'post': get_object_or_404(Post, slug=slug)
+    })
