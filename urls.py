6346a1bbb849e894e96f6efe35cb36d368a75f8a
Russell Myers <mezner@russellmyers.com>
2012-02-27 23:00:41 -0500

    Adding blog urls to base urls

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'russellmyers.views.home', name='home'),
-    # url(r'^russellmyers/', include('russellmyers.foo.urls')),
+    url(r'^blog/', include('blog.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
