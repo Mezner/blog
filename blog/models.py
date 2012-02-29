71ba3b333532da9e36999969f0cf028c2d35f9b2
Russell Myers <mezner@russellmyers.com>
2012-02-28 20:49:04 -0500

    Implementing tinymce for post body.

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 class Post(models.Model):
     title = models.CharField(max_length=120, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    body = models.TextField()
+    body = tinymce_models.HTMLField()
     posted = models.DateField(auto_now=True)
 
     def __unicode__(self):
--- settings.py
+++ settings.py
@@ -118,7 +118,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'blog'
+    'blog',
+    'tinymce'
 )
 
 # A sample logging configuration. The only tangible logging
