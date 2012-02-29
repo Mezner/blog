3484995b797faa203a6bd1ff4962dc13ff39cce0
Russell Myers <mezner@russellmyers.com>
2012-02-29 07:13:04 +0300

    Changing database path to 'production' value. Adding template directory for 'production'.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/mezner/src/russellmyers/rm.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/mezner/russellmyers/rm.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -105,6 +105,7 @@ ROOT_URLCONF = 'russellmyers.urls'
 TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
     '/home/mezner/src/russellmyers/templates',
+    '/home/mezner/russellmyers/templates',
 )
 
 INSTALLED_APPS = (
