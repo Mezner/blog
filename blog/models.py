17a502aa5d48f9caa4434e005b76ad22678e8956
Russell Myers <mezner@russellmyers.com>
2012-02-29 22:46:40 -0500

    Changing slugfield to auto slug from the django-autoslug package.

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from tinymce import models as tinymce_models
+from autoslug import AutoSlugField
 
 class Post(models.Model):
     title = models.CharField(max_length=120, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from='title', max_length=50, unique=True)
     body = tinymce_models.HTMLField()
     posted = models.DateField(auto_now=True)
 
