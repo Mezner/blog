228c3f55a7caae5f1cb76b209c90d4afeafcce9d
Russell Myers <mezner@russellmyers.com>
2012-03-05 08:35:15 +0300

    Adding base url to urls.py. This is really just a redirect.

--- urls.py
+++ urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'russellmyers.views.home', name='home'),
     url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('blog.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
