e20f1d683f0c0181082857d6fd48a32a6c91a74e
Russell Myers <mezner@russellmyers.com>
2012-03-11 21:26:50 -0400

    Adding RequestContext to blog post view. Also adding CSRF to form and appropriate post location.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 from blog.models import Post
 
 def index(request):
@@ -7,7 +8,8 @@ def index(request):
     })
 
 def view_post(request, slug):
-    return render_to_response('blog/view_post.html', {
+    return render_to_response('blog/view_post.html',
+    context_instance = RequestContext(request, {
         'post': get_object_or_404(Post, slug=slug),
         'posts': Post.objects.order_by('-posted')[:10]
-    })
+    }))
