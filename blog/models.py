e1111a2cc3bc9e19cf8f32c48ba7552921f70b5e
Russell Myers <mezner@russellmyers.com>
2012-03-26 13:57:42 -0400

    Turning auto_now off and auto_now_add to true for posted time. This was not meant to represent the time in which something was last edited.

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=120, unique=True)
     slug = AutoSlugField(populate_from='title', max_length=50, unique=True)
     body = tinymce_models.HTMLField()
-    posted = models.DateField(auto_now=True)
+    posted = models.DateField(auto_now_add=True, auto_now=False)
 
     def __unicode__(self):
         return self.title
